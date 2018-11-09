6f852af6d6b0244cafece0ed45e30a932c513d2a
Elden Humberto Cruz Vera <elden.cruz17@tectijuana.edu.mx>
2018-11-09 02:08:55 -0800

    holi
    aqui esta la tarea

---
+++ E4-1.Cruz Vera Elden Humberto/E4-1.Cruz Vera Elden Humberto/Nodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4_1.Cruz_Vera_Elden_Humberto
+{
+    class Nodo
+    {
+        public int info;
+        public Nodo izq, der;
+    }
+}
