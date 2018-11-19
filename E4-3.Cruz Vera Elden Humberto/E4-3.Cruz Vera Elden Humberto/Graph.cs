7cc492aa9beeb6e8d5477a5e81fa6d24ea5fcf99
Elden Humberto Cruz Vera <elden.cruz17@tectijuana.edu.mx>
2018-11-19 12:22:28 -0800

    E4-3.Cruz Vera
    grafo con lazo

---
+++ E4-3.Cruz Vera Elden Humberto/E4-3.Cruz Vera Elden Humberto/Graph.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4_3.Cruz_Vera_Elden_Humberto
+{
+    public class Graph
+    {
+        // Contiene los nodos hijos de cada vertice del grafo
+        // Asumiendo que los vertices comienzan del 0
+        private List<int>[] nodosHijos;
+
+        // Construye un grafo de x tamañao
+        // Tamaño representa el numero de vertices
+        public Graph(int Tamaño)
+        {
+            this.nodosHijos = new List<int>[Tamaño];
+            for (int i = 0; i < Tamaño; i++)
+            {
+                // asigna una lista vacia de vertices adjacentes
+                this.nodosHijos[i] = new List<int>();
+            }
+        }
+
+        //onstruye un grafo basado en las listas que se generan de los nodos sucesores/adyacentes de cada nodo
+        public Graph(List<int>[] nodosHijos)
+        {
+            this.nodosHijos = nodosHijos;
+        }
+
+        // Regresa el tamaño del grafo, osea la cantidad de nodos
+        public int Tamaño
+        {
+            get { return this.nodosHijos.Length; }
+        }
+
+
+        // Este metodo checa si hay conexion entre dos nodos, recibe dos parametros, un para el primer nodo
+        // y el otro para el segundo nodo, si tienen conexion regresa True, de lo contrario False
+        public bool TieneConexion(int u, int v)
+        {
+            bool TieneConexion = nodosHijos[u].Contains(v);
+            return TieneConexion;
+        }
+
+        // Regresa los sucesores/hijos del nodo deseado
+        public IList<int> ObtenerNodos(int v) 
+        {
+            return nodosHijos[v];
+        }
+
+        // los ultimos metodos no son necesarios, pero me parecieron utiles para entender el codigo
+    
+    }
+}
---
+++ E4-3.Cruz Vera Elden Humberto/E4-3.Cruz Vera Elden Humberto/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E4_3.Cruz_Vera_Elden_Humberto
+{
+    class GraphComponents
+    {  // Aqui genera el grafo con la primer ruta, ya despues de eso profe le intente pero
+        // no me salian bien las otras trayectorias simples, quizas debi usar un metodo BFS
+        static Graph graph = new Graph(new List<int>[] {
+        new List<int>() {0, 1},     // Nodos adyacentes del  vertice 0
+        new List<int>() {0, 2, 6}, // Nodos adyacentes del  vertice 1
+        new List<int>() {1,3,6},    // Nodos adyacentes del  vertice 2
+        new List<int>() {2,4,5},     // Nodos adyacentes del  vertice 3
+        new List<int>() {3,5},     // Nodos adyacentes del  vertice 4
+        new List<int>() {3,4,6},      // Nodos adyacentes del  vertice 5
+        new List<int>() {1, 2, 5}  // Nodos adyacentes del  vertice 6
+    });
+
+        static Graph graph1 = new Graph(new List<int>[] { 
+        new List<int>() {0, 1},     // Nodos adyacentes del  vertice 0
+        new List<int>() {3,5},     // Nodos adyacentes del  vertice 4  
+        new List<int>() {0, 2, 6}, // Nodos adyacentes del  vertice 1
+        new List<int>() {1,3,6},    // Nodos adyacentes del  vertice 2
+        new List<int>() {2,4,5},     // Nodos adyacentes del  vertice 3
+        new List<int>() {3,4,6},      // Nodos adyacentes del  vertice 5
+        new List<int>() {1, 2, 5},  // Nodos adyacentes del  vertice 6
+        
+    });
+
+
+        static bool[] visited = new bool[graph.Tamaño]; // Arreglo que contiene valores True/False si se ha visitado el nodo
+        // contiene el tamaño de nuestro grafo
+
+        static bool[] visited1 = new bool[graph1.Tamaño];
+
+
+        static void TraverseDFS(int v) // Aqui se usa un metodo DFS para poder imprimir la conexion entre los nodos
+        {
+            if (!visited[v]) // Si no se ha visitado el nodo visited se hace verdadero porque ya paso por ese nodo
+            {
+                Console.Write(v + " ");
+                visited[v] = true;
+                foreach (int child in graph.ObtenerNodos(v)) // Dentro de un ciclo ponemos un metodo recursivo para que se
+                    // se visiten los demas nodos
+                {
+                    TraverseDFS(child);
+                }
+            }
+        }
+
+        static void TraverseDFS1(int v) // lo mismo que el mismo metodo anterior, excepto que aqui no me salio xd
+        {
+            if (!visited1[v])
+            {
+                Console.Write(v + " ");
+                visited1[v] = true;
+                foreach (int child in graph1.ObtenerNodos(v))
+                {
+                    TraverseDFS1(child);
+                }
+            }
+        }
+
+  
+
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("0 = A");
+            Console.WriteLine("1 = B");
+            Console.WriteLine("2 = C");
+            Console.WriteLine("3 = D");
+            Console.WriteLine("4 = E");
+            Console.WriteLine("5 = F");
+            Console.WriteLine("6 = G");
+            Console.WriteLine("Nodos conectados del grafo y primer trayectoria: ");
+
+            // En este metodo del main mandamos como parametro la v para que el metodo TravserseDFS pueda ser recursivo
+            for (int v = 0; v < graph.Tamaño; v++)
+            {
+                if (!visited[v])
+                {
+                    TraverseDFS(v);
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Segunda trayectoria");
+            for (int v = 0; v < graph1.Tamaño; v++)
+            {
+                if (!visited1[v])
+                {
+                    TraverseDFS1(v);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.Write("Presione una tecla para continuar ");
+            Console.ReadKey();
+        }
+    }
+}
